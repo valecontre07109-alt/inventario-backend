a5c6089caa25af96362f165a84d248807c728cc7
valecontre07109-alt <valecontre07109@gmail.com>
2026-09-09 14:19:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -669,7 +669,7 @@ app.get("/materiales", verificarToken, (req, res) => {
     });
 });
 
-const PUERTO = process.env.PORT || 3000;
-app.listen(PUERTO, () => {
-    console.log(`Servidor en el puerto ${PUERTO}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor en el puerto ${PORT}`);
 });
