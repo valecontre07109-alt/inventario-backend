0f9220c9dd1baffd8235d2698fd65fd1c78b0b39
valecontre07109-alt <valecontre07109@gmail.com>
2026-09-10 15:57:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ const conexion = mysql.createConnection({
     host: process.env.MYSQLHOST || "localhost",
     user: process.env.MYSQLUSER || "root",
     password: process.env.MYSQLPASSWORD || "",
-    database: process.env.MYSQLDATABASE || "inventario_db",
+    database: process.env.MYSQLDATABASE || "railway",
     port: process.env.MYSQLPORT || 3306
 });
 
